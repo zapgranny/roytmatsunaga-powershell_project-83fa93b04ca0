28fbe41240009d554aab46d5e1f3b60cf7c23c09
zapgranny <zapgranny@hotmail.com>
2013-11-21 12:31:28 -0700

    This should be helpful
    G

--- script.ps1
+++ script.ps1
@@ -15,6 +15,8 @@ Gather information about Windows computers on the network
 http://blogs.technet.com/b/askds/archive/2010/02/04/inventorying-computers-with-ad-powershell.aspx
 #>
 
-import-module ActiveDirectory
+Import-Module activedirectory #This has to be somewhere on the computer... I think I may need to do this part on Windows Server 2008
 
-Get-Help Get-ADComputer -Full
+#Get-Help Get-ADComputer -Full
+
+#Get-ADComputer -Filter * -Property * | Format-Table Name,OperatingSystem,OperatingSystemServicePack,OperatingSystemVersion -Wrap –Auto
