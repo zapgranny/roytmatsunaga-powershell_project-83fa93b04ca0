a3c08433b4a4c1154c7a354557cf7ae9e702e70c
zapgranny <zapgranny@hotmail.com>
2013-11-21 17:50:34 -0700

    Turns out my other code needed to be changed...
    G

--- script.ps1
+++ script.ps1
@@ -12,11 +12,17 @@ Team B - Project 3: Powershell Scripting
 
 <#
 Gather information about Windows computers on the network
-http://blogs.technet.com/b/askds/archive/2010/02/04/inventorying-computers-with-ad-powershell.aspx
+
+
+•	You should primarily use WMI classes to gather information.  
+•	Your script should be as comprehensive as possible.
+•	Example information may include BIOS information, programs installed on the computer, current programs running, accounts that exist on the computer, etc.
+•	While the script by default should run on the machine where it is executed, it should also be able to be executed for a remote machine (assuming you have rights).
+o	You can create a test environment using the VMs you have setup for this class;
+•	Your script should be able to be called from a PowerShell console and appropriate parameters passed to it.
+•	Feel free to use the internet to find potential solutions; however, you should not copy scripts exactly as you find them online.  You should write them yourself but can be guided by what you find. 
+•	Make sure that you comment your script.
+•	Your script should save the output of the information that it gathers to a file. You can determine the type and organization of the file.
+
 #>
 
-Import-Module activedirectory #This has to be somewhere on the computer... I think I may need to do this part on Windows Server 2008
-
-#Get-Help Get-ADComputer -Full
-
-#Get-ADComputer -Filter * -Property * | Format-Table Name,OperatingSystem,OperatingSystemServicePack,OperatingSystemVersion -Wrap –Auto
